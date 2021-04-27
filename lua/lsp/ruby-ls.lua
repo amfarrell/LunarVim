4047e73d134268b1976673442b5fe773927913ff
Andrew M. Farrell <andrew.simplybusiness.co.uk>
2021-04-27 05:22:55 +0100

    Point to the right solargraph path

--- lua/lsp/ruby-ls.lua
+++ lua/lsp/ruby-ls.lua
@@ -1,5 +1,7 @@
 -- If you are using rvm, make sure to change below configuration
 require'lspconfig'.solargraph.setup {
+    --cmd = { DATA_PATH .. "~/.rbenv/shims/solargraph", "--stdio" },
+    --cmd = { DATA_PATH .. "/lspinstall/ruby/solargraph/bin/solargraph", "--stdio" },
     cmd = { DATA_PATH .. "/lspinstall/ruby/solargraph/solargraph", "--stdio" },
     on_attach = require'lsp'.common_on_attach,
     handlers = {
